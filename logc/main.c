592cce76128f440c87c82d0e6e4861b2a1ce6316
tonylxc <cooniur@gmail.com>
2013-10-11 01:20:16 -0400

    Initial Commit

---
+++ logc/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  logc
+//
+//  Created by Tongliang Liu on 10/11/13.
+//  Copyright (c) 2013 cooniur. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	printf("Hello, World!\n");
+    return 0;
+}
+
